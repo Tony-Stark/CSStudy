16562592a638f1f5548d29623a6814bcce94420a
Tony Stark <sz.unsc@gmail.com>
2016-03-26 17:35:16 +0300

    Update and rename MaximumSuarray.cpp to MaximumSubarray.cpp

--- Hackerrank/MaximumSubarray.cpp
+++ Hackerrank/MaximumSubarray.cpp
@@ -1,9 +1,18 @@
+ /**
+    Maximum Subarray Problem - Hackerrank
+    maximumsubarray.cpp
+    Purpose: Solves the Maximum Subarray problem
+
+    @author thejedi
+    @version 1.0 26/03/2016
+    */
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
+//this function will find the maximum sum of the maximal subarrat
 int conti_sol(int arr[],int n){
     int curr=0;
     int tot=0;  
@@ -22,6 +31,7 @@ int conti_sol(int arr[],int n){
     
    return tot;
 }
+//this function finds the maximum of the maximum combination of elements, regardless of position in the array.
 int non_conti_sol(int arr[],int n){
     int sum=0;
     for (int i=0; i<n;++i){
@@ -35,13 +45,12 @@ int non_conti_sol(int arr[],int n){
     }
     return sum;
 }
-int scanarr(int arr[],int n);
+int scanarr(int arr[],int n); //handles scanning of arrays
 int main() {
-    int cases;
-    
+    int cases; //number of test cases
     scanf("%d",&cases);
-    int sols1[cases];
-    int sols2[cases];
+    int sols1[cases]; //is used to store solution for the first problem
+    int sols2[cases]; //is used to store solution for the second problem
     for (int i=0; i<cases;i++){
         int size=0;
         scanf("%d",&size);
@@ -50,11 +59,12 @@ int main() {
         sols1[i]=conti_sol(arr,size);
         sols2[i]=non_conti_sol(arr,size);
     }
-    for(int i=0 ;i<cases;i++)
+    for(int i=0 ;i<cases;i++) //prints out solutions in the correct format
         cout<<sols1[i]<<" "<<sols2[i]<<endl;
         
     return 0;
 }
+/handles the scanning of arrays
 int scanarr(int arr[],int n){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
