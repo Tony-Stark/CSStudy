db127c01acb85a40082cb9ac545257dc1d121bed
Tony Stark <sz.unsc@gmail.com>
2016-03-29 12:14:20 +0300

    Hackerearth

---
+++ Hackerearth/Practice/p1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+int c=0;
+int count=0;
+int iter=1;
+cin>>c;
+while(c/(pow(5,iter))>0){
+count+=c/(pow(5,iter));
+	++iter;
+}
+cout<<count<<endl;
+}
