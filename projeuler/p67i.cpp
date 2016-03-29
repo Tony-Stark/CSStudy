ac910559166cd882f47a50a42924effff661bcd7
Tony Stark <sz.unsc@gmail.com>
2016-03-29 16:57:08 +0300

    Update p67i.cpp

--- projeuler/p67i.cpp
+++ projeuler/p67i.cpp
@@ -4,17 +4,14 @@
 using namespace std;
 
 int main(){
-    ifstream fil e;
+    ifstream file;
     file.open("triangle.txt");
-    if(!file.is_open()){
+    if(!file.is_open())
         cout<<"error opening file";
-        }
     else{
         string line;
-        for(int i=0;i<100<i++){
+        for(int i=0;i<100<i++)
             getline(file,line);
         }
-        }
- 
  return 0;
 }
