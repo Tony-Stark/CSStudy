a13d434833436c3deed83fa5b320ff898252384c
tony-stark <sz.unsc@gmail.com>
2016-03-29 16:53:09 +0300

    inputoutput

--- projeuler/p67i.cpp
+++ projeuler/p67i.cpp
@@ -1,1 +1,20 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+    ifstream fil e;
+    file.open("triangle.txt");
+    if(!file.is_open()){
+        cout<<"error opening file";
+        }
+    else{
+        string line;
+        for(int i=0;i<100<i++){
+            getline(file,line);
+        }
+        }
+ 
+ return 0;
+}
