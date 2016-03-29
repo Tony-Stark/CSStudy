fac415a6f2e8562c0104eacd211ce8a25fdc9904
tony-stark <sz.unsc@gmail.com>
2016-03-29 16:50:10 +0300

    newbeanch

--- Hackerearth/Practice/p1.cpp
+++ Hackerearth/Practice/p1.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int main()
 { // hi
-int c=0;
-int count=0;
+int c=0;int count=0;
 int iter=1;
 cin>>c;
 while(c/(pow(5,iter))>0){
 count+=c/(pow(5,iter));
 	++iter;
 }
-cout<<count<<endl;
+       cout<<count<<endl;
 }
